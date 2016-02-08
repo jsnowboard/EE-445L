5f4b23b74e4f6279899f15bd164b3f479d5db18a
unknown <jbullfrog1@gmail.com>
2016-02-08 12:30:22 -0600

    Added matlab file for graphing Lab 2

--- Lab2/Lab2.h
+++ Lab2/Lab2.h
@@ -88,3 +88,18 @@ void ADC0_InitAllTriggerSeq3(uint32_t channelNum);
 // Input: none
 // Output: 12-bit result of ADC conversion
 uint32_t ADC0_InSeq3(void);
+
+//************* ST7735_Line********************************************
+//  Draws one line on the ST7735 color LCD
+//  Inputs: (x1,y1) is the start point
+//          (x2,y2) is the end point
+// x1,x2 are horizontal positions, columns from the left edge
+//               must be less than 128
+//               0 is on the left, 126 is near the right
+// y1,y2 are vertical positions, rows from the top edge
+//               must be less than 160
+//               159 is near the wires, 0 is the side opposite the wires
+//        color 16-bit color, which can be produced by ST7735_Color565() 
+// Output: none
+void ST7735_Line(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color);
+
---
+++ Lab2/fixed.d
@@ -0,0 +1,5 @@
+fixed.o: fixed.c
+fixed.o: fixed.h
+fixed.o: C:\Program Files\Keil\ARM\RV31\Inc\stdio.h
+fixed.o: C:\Program Files\Keil\ARM\RV31\Inc\stdint.h
+fixed.o: ST7735.h
