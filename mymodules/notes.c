a896ed711d258befc1c62e53660c7507c4b2671f
unknown <jbullfrog1@gmail.com>
2016-02-29 09:24:57 -0600

    Added note definitions

--- mymodules/notes.c
+++ mymodules/notes.c
@@ -2,5 +2,23 @@
 // Runs on TM4C123
 // Ty Winkler
 // Feb 23, 2016
+// Modified by Jeremiah Feb 29, 2016
 
-#define A 10
+#define C4 277
+#define D4low 293
+#define E4flat 311
+#define E4 329
+#define F4 349
+#define G4flat 370
+#define G4 391
+#define A4flat 415
+#define A4 440
+#define B4flat 466
+#define B4 494
+#define C5 523
+#define D5flat 554
+#define D5 587
+#define E5flat 622
+#define E5 659
+#define F5 698
+#define G5flat 740
